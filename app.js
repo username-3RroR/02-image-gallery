f054ea420d8c493b1468a41e8ef1c32178d5457e
maggie <maggie.duca@student.norwichuni.ac.uk>
2025-06-06 16:35:49 +0100

    css

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 console.log('Hello New York!');
+
+const bigImageContainer = document.getElementById('bigImageContainer');
+const navImageContainer = document.getElementById('navImageContainer');
+
+const images = document.getElementById('images').children;
+/*
+// the function to call back for to sync with the little image navigation container
+function openBigImage(openThisImage) {
+	bigImageContainer.innerHTML = '';
+
+	const bigImage = document.createElement('img');
+
+	bigImage.src = openThisImage.src;
+	bigImage.alt = openThisImage.alt;
+
+	bigImageContainer.appendChild(bigImage);
+}
+
+function runImg() {
+	images.forEach(function (image, index) {
+		let imgTag = document.createElement('img');
+
+		imgTag.src = image.src;
+		imgTag.alt = image.alt;
+
+		imgTag.addEventListener('click', function () {
+			openBigImage(image);
+
+			currentImgIndex = index;
+		});
+	});
+} 
+*/
