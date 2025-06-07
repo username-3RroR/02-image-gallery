a2600b43bcde0b9c5ad223e0bac8c99e0478e472
maggie <maggie.duca@student.norwichuni.ac.uk>
2025-06-07 19:03:01 +0100

    I give up

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 02-image-gallery
+
+--What requirements did you achieve?
+I added alt text to images and tabindex to allow screen readers to select and read the alt texts. It works with my screen reader.
+
+--Were there any requirements or goals that you were unable to achieve?
+Buttons are not working, no keyboard interaction, images do not adjust for different screen sizes.
+I tried messing with JS to make the buttons work but couldn't. I understand parts of JS on their own (e.g. functions, arrays, methods, objects), I can't get my head around combining them to do something.
+I added srcset for different screen sizes (the different images are in the img-assets folder), but it messed up JS. The thumbnail images were not displaying, only the alt text, so I deleted everything.
+
+--If so, what was it that you found difficult about these tasks?
+Java Script - I do not understand how to use it for a website, how to put all the parts together to make things work.
--- app.js
+++ app.js
@@ -3,19 +3,36 @@ console.log('Hello New York!');
 const bigImageContainer = document.getElementById('bigImageContainer');
 const images = document.getElementById('images').children;
 
-//let currentImageIndex = 0;
+const previousBtn = document.getElementById('previous');
+const nextBtn = document.getElementById('next');
 
-function selectImages() {
+let currentImageIndex = 0;
+
+function selectImages(index) {
 	for (const image of images) {
 		image.addEventListener('click', function () {
 			bigImages(image);
 
-			//currentImageIndex = index;
+			if (index >= images.length) {
+				currentImageIndex = 0;
+			} else if (index < 0) {
+				currentImageIndex = images.length - 1;
+			} else {
+				currentImageIndex = index;
+			}
 		});
 	}
 }
 
 selectImages();
+
+previousBtn.addEventListener('click', function () {
+	selectImages(currentImageIndex - 1);
+});
+
+nextBtn.addEventListener('click', function () {
+	selectImages(currentImageIndex + 1);
+});
 
 function bigImages(img) {
 	bigImageContainer.innerHTML = '';
